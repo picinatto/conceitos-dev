e12c81c2d8b80ac54a0b1c9657dc62abb1f82cf9
Ricardo Picinatto <ederli.picinatto@gmail.com>
2020-04-07 17:38:04 -0300

    create the list, add, update and delete routes for the backend

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,9 +1,77 @@
 const express = require('express');
+const { uuid } = require('uuidv4');
 
 const app = express();
 
-app.get('/', (req, res) => {
-  return res.json({message: 'Hello from the backend!'});
+app.use(express.json());
+
+const projects = [];
+
+app.get('/projects', (request, response) => {
+  // const { title, owner } = request.query;
+
+  // console.log(title);
+  // console.log(owner);
+
+  return response.json(projects);
+});
+
+app.post('/projects', (request, response) => {
+  const { title, owner } = request.body;
+
+  const project = { id: uuid(), title, owner };
+
+  projects.push(project);
+
+  return response.json(project);
+});
+
+app.put('/projects/:id', (request, response) => {
+  // Get the id from the query params
+  const { id } = request.params;
+
+  // Get the title and owner data from the request body
+  const { title, owner } = request.body;
+  
+  // Look for which index the project is based on the id provided
+  const projectIndex = projects.findIndex(project => project.id === id);
+
+  // Check if the provided id is valid (not found is -1)
+  if(projectIndex < 0) {
+    // if not valid, return status 400 not found
+    return response.status(400).json({ error:'Project not found!' });
+  }
+
+  // Create a new project object (with the new data but same id)
+  const project = {
+    id,
+    title,
+    owner,
+  };
+
+  // Change the data for the project by the index to the new one
+  projects[projectIndex] = project;
+
+  return response.json(project);
+});
+
+app.delete('/projects/:id', (request, response) => {
+  // Get the id from the query params
+  const { id } = request.params;
+  
+  // Look for which index the project is based on the id provided
+  const projectIndex = projects.findIndex(project => project.id === id);
+
+  // Check if the provided id is valid (not found is -1)
+  if(projectIndex < 0) {
+    // if not valid, return status 400 not found
+    return response.status(400).json({ error:'Project not found!' });
+  }
+
+  // If found remove the project by the index found
+  projects.splice(projectIndex, 1);
+
+  return response.status(204).send();
 });
 
 app.listen(3333, () => {
