0e6e9a4717aa0875d503eef5080fe83f37be0282
Ricardo Picinatto <ederli.picinatto@gmail.com>
2020-04-07 18:13:35 -0300

    add the log requests and validate id middleware

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,11 +1,49 @@
 const express = require('express');
-const { uuid } = require('uuidv4');
+const { uuid, isUuid } = require('uuidv4');
 
 const app = express();
 
 app.use(express.json());
 
 const projects = [];
+
+// Create the middleware tp track the requests log
+function logRequests(request, response, next) {
+  // Get the data from the request
+  const { method, url } = request;
+
+  // Builds the log information about the request
+  const logLabel = `[${method.toUpperCase()} ${url}]`;
+  
+  // Console log time starts
+  console.time(logLabel);
+  
+  // Continue the flow
+  next();
+
+  // The flow returns, stop the watch and log the request data
+  console.timeEnd(logLabel);
+};
+
+// Create the middleware that validates if the id is correct
+function validateProjectId(request, response, next) {
+  const { id } = request.params;
+
+  // Check if the id is valid
+  if(!isUuid(id)) {
+    // Retorn 400 status and the request is stopped
+    return response.status(400).json({ error: 'Invalid project ID.'});
+  }
+
+  // Return to the normal flow
+  return next();
+}
+
+// Is going to be triggered in all application's requests
+app.use(logRequests);
+
+// Is going to be triggered on requests with the id param
+app.use('/projects/:id', validateProjectId)
 
 app.get('/projects', (request, response) => {
   const { title } = request.query;
