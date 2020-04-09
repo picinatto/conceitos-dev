50e273e819b930429d46408b30472c994796b6d2
Ricardo Picinatto <ederli.picinatto@gmail.com>
2020-04-09 15:33:23 -0300

    Add cors to backend communication back and front, display data

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,28 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import api from './services/api';
 
 import './App.css';
-
-import backgroundImage from './assets/background-image.jpeg'
 
 import Header from './components/Header';
 
 function App() {
-  const [projects, setProjects] = useState(['Desenvolvmento de app', 'Frontend app']);
+  const [projects, setProjects] = useState([]);
+
+  useEffect(() => {
+    api.get('projects').then(response => {
+      setProjects(response.data);
+    });
+  }, []);
 
   function handleAddProject() {
-    setProjects([...projects, `Novo projeto ${Date.now()}`]) // Imutabilidade: O array esta sendo recriado, não alterado
-    //projects.push(`Novo projeto ${Date.now()}`); // Aqui o array é apenas alterado
+    setProjects([...projects, `Novo projeto ${Date.now()}`]);
   }
 
   return(
     <>
       <Header title="Projects" />
-      <img src={backgroundImage} />
       <ul>
-        {projects.map(project => <li key={project}>{project}</li>)}
+        {projects.map(project => <li key={project.id}>{project.title}</li>)}
       </ul>
       <button type="button" onClick={handleAddProject}>Adicionar projeto</button>
     </>
