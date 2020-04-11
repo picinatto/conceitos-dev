be9e7c56f7082a0bb1871f6bc52a9cf6f8cdfb7b
Ricardo Picinatto <ederli.picinatto@gmail.com>
2020-04-11 11:49:12 -0300

    Add communication with the backend from the mobile client

--- mobile/src/index.js
+++ mobile/src/index.js
@@ -1,7 +1,18 @@
-import React from 'react';
-import { View, Text, StyleSheet, StatusBar, StatusBarIOS } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { View, Text, StyleSheet, StatusBar } from 'react-native';
+
+import api from './services/api';
 
 export default function App() {
+  const [projects, setProjects] = useState([]);
+
+  useEffect(() => {
+    api.get('projects').then(response => {
+      console.log(response.data);
+      setProjects(response.data);
+    });
+  }, []);
+
   return(
     <>
       <StatusBar barStyle="ligh-content" backgroundColor='#7159c1'/>
