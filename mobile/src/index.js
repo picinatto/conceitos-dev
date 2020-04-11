7b36c37357a606ec60eb9c1f37bbdad4e6418649
Ricardo Picinatto <ederli.picinatto@gmail.com>
2020-04-11 10:59:38 -0300

    improve the global style and status bar

--- mobile/src/index.js
+++ mobile/src/index.js
@@ -1,13 +1,27 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, StatusBar, StatusBarIOS } from 'react-native';
 
 export default function App() {
-  return <View style={styles.container} />
+  return(
+    <>
+      <StatusBar barStyle="ligh-content" backgroundColor='#7159c1'/>
+      <View style={styles.container}>
+        <Text style={styles.title}>Hello GoStack</Text>
+      </View>
+    </>
+  );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#7159c1' 
+    backgroundColor: '#7159c1',
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  title: {
+    color: '#FFF',
+    fontSize: 32,
+    fontWeight: 'bold'
   },
 });
