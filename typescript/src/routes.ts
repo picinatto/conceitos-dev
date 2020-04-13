38c24b49456cda0cfd5fe39a8b4bbe5e0dee213e
Ricardo Picinatto <ederli.picinatto@gmail.com>
2020-04-13 17:01:36 -0300

    Add array type

--- typescript/src/routes.ts
+++ typescript/src/routes.ts
@@ -8,7 +8,12 @@ export function helloWorld(request: Request, response: Response) {
   const user = createUser({
     name:'Ricardo',
     email: 'ricardo@gmail.com', 
-    password: '13216'
+    password: '13216',
+    techs: [
+        'Node.js',
+        'ReactJS',
+        { title: 'Javascript', experience: 100 },
+      ],
   });
   // Gets easier to user the fields using Intelisense
   return response.json({ message: `Hello ${user.name}` });
--- typescript/src/services/CreateUser.ts
+++ typescript/src/services/CreateUser.ts
@@ -1,18 +1,26 @@
+// Create the interface to use the TechObject in Techs
+interface TechObject {
+  title: string;
+  experience: number;
+}
+
 // Create the interface in order to facilitate the creation of an 
 // object that will hold the data and show the items in Intelisense
 interface CreateUserData {
   name?: string; // The ? marks that the field is optional
   email: string;
   password: string;
+  techs: Array<string | TechObject>; //If was only string could use string[];
 }
 
 export default function createUser(
-  {name, email, password} // List all the fields
+  {name, email, password, techs} // List all the fields
   :CreateUserData ) { // Link to the interface to get the types and rules
   const user = {
     name, 
     email,
     password,
+    techs
   }
   // Return the data
   return user;
