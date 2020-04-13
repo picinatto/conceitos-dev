6226dbd55aac3701b812def5f5d7de8d94335c51
Ricardo Picinatto <ederli.picinatto@gmail.com>
2020-04-13 15:04:00 -0300

    Add tsconfig to transpile to JS file

--- typescript/src/index.js
+++ typescript/src/index.js
@@ -1,7 +1,10 @@
 "use strict";
-exports.__esModule = true;
-var express_1 = require("express");
-var app = express_1["default"]();
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+var express_1 = __importDefault(require("express"));
+var app = express_1.default();
 app.get('/', function (request, response) {
     return response.json({ message: 'Hello World!' });
 });
